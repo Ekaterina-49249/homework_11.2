80b09b77f07b37ea743b8d711562e48612ea7c3e
Katya Ivanova <ivanova49249@mail.ru>
2023-08-07 13:16:41 +0700

    added func_upper_2 + docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def func_upper(input_str: str):
+    """ Функция переводит строку в верхний регистр"""
     return input_str.upper()
+
+def func_upper_2(input_str: str):
+    """ Функция переводит первую букву строки в верхний регистр"""
+    return input_str.capitalize()
